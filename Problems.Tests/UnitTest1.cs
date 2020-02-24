fa6df24b9ccbdc9838ab74bbff4d1ec142aed08d
Farai Jumbe <faraij55@gmail.com>
2020-02-24 23:34:04 +0200

    single test modification

--- Problems.Tests/UnitTest1.cs
+++ Problems.Tests/UnitTest1.cs
@@ -14,18 +14,22 @@ namespace Problems.Tests
         {
             string a = Program.Prefix("hello");
             Assert.AreEqual("5,1:hello", a);
+
+            string b = Program.Prefix("");
+            Assert.AreEqual("0,0:", b);
+
+             string c = Program.Prefix("what ... did you say??     ");
+            Assert.AreEqual("27,5:what ... did you say??     ", c);
+
+            string d = Program.Prefix("   hello   ");
+            Assert.AreEqual("11,1:", d);
+
+            string e = Program.Prefix("what happened   here");
+            Assert.AreEqual("20,3:", b);
+
+            string f = Program.Prefix(... ...);
+            Assert.AreEqual("7,2:", b);
         }
-        [Test]
-        public void Test2()
-        {
-            string a = Program.Prefix("");
-            Assert.AreEqual("0,0:", a);
-        }
-        [Test]
-        public void Test3()
-        {
-            string a = Program.Prefix("what ... did you say??     ");
-            Assert.AreEqual("27,5:what ... did you say??     ", a);
-        }
+        
     }
 }
